f635b2336d9e3809663d40d6f7699ca32256381d
unknown <maruf.amin57@gmail.com>
2024-02-08 20:32:12 -0500

    meme generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {GeneratorComponent} from "./generator/generator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, GeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
